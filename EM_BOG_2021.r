c4ab260e9b48bcf7aae316411bd287616df1c9b7
SChaparro16 <schapalo1608@gmail.com>
2026-03-26 16:16:51 -0500

    Versión 0.4 - Paquetes

--- EM_BOG_2021.r
+++ EM_BOG_2021.r
@@ -1,3 +1,28 @@
 # Encuesta multipropósito 2021
-#---- Dependencias ----#
-#---- Datos ----#
+# ---- Dependencias ---- #
+
+# Lectura de archivos
+library(readr)
+library(readxl)
+# Tablas/Estadísticas
+library(modelsummary)
+library(estimatr)
+library(stargazer) # library(kableExtra)
+library(e1071)
+# Gráficos
+library(ggplot2)
+# Manipulación de datos
+library(dplyr) # library(tidyverse)
+library(naniar)
+library(fastDummies)
+library(stringr)
+library(magrittr)
+library(missForest)
+# Encuestas
+library(srvyr)
+
+# ---- Datos ---- #
+# ---- Limpieza de datos --- #
+# ---- Diseño de encuesta ---- #
+# ---- Estimaciones ---- #
+# ---- Gráficos ---- #
