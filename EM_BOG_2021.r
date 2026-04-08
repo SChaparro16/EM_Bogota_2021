51f0ba6f4984d9feb382761645b7dc17f97ab396
SChaparro16 <schapalo1608@gmail.com>
2026-04-08 16:18:28 -0500

    Correcciones de forma

--- EM_BOG_2021.r
+++ EM_BOG_2021.r
@@ -1,5 +1,5 @@
 # Encuesta multipropósito 2021
-# ---- Dependencias ---- #
+# ---- Dependencias ----
 
 # Lectura de archivos
 library(readr)
@@ -7,22 +7,32 @@ library(readxl)
 # Tablas/Estadísticas
 library(modelsummary)
 library(estimatr)
-library(stargazer) # library(kableExtra)
+library(stargazer)
+library(kableExtra)
 library(e1071)
 # Gráficos
 library(ggplot2)
 # Manipulación de datos
-library(dplyr) # library(tidyverse)
-library(naniar)
+library(dplyr)
+library(tidyverse)
+# library(naniar) - No instalado
 library(fastDummies)
 library(stringr)
 library(magrittr)
-library(missForest)
+# library(missForest) - No instalado
 # Encuestas
-library(srvyr)
+## library(srvyr) - No instalado
 
 # ---- Datos ---- #
-# ---- Limpieza de datos --- #
-# ---- Diseño de encuesta ---- #
-# ---- Estimaciones ---- #
-# ---- Gráficos ---- #
+
+setwd("/home/scha/Documentos/Proyectos/EM_Bogota_2021/Data")
+
+# Capítulo A - Identificación
+cap_a_id <- read.csv2("CAP_A_Identificación.csv",fileEncoding = "latin1")
+# Capítulo B - Datos de la vivenda y su entorno
+cap_b_vivenda <- read.csv2("CAP_B_Data_Vivienda.csv",fileEncoding = "latin1")
+
+# ---- Limpieza de datos ----
+# ---- Diseño de encuesta ----
+# ---- Estimaciones ----
+# ---- Gráficos ----
